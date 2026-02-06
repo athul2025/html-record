3304914937b793f54e79d405f1a256a59eb279fc
Athul Krishna R <athul1296krishna@gmail.com>
2026-02-06 09:36:20 +0530

    Update 17.php

--- 17.php
+++ 17.php
@@ -1,27 +1,40 @@
-<html>
-<head>
-    <title>Factorial of a Number</title>
-</head>
-<body>
+import java.util.Scanner;
 
-<h2>Factorial of a Number</h2>
+public class StringManipulation {
+    public static void main(String[] args) {
 
-<form method="post">
-    Enter a number: <input type="text" name="num">
-    <input type="submit" value="Find Factorial">
-</form>
+        Scanner sc = new Scanner(System.in);
 
-<?php
-$num = $_POST['num'];
-$fact = 1;
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
 
-for ($i = 1; $i <= $num; $i++) {
-    $fact = $fact * $i;
+        System.out.println("Length: " + str1.length());
+        System.out.println("Concat: " + str1.concat(str2));
+        System.out.println("charAt(1): " + str1.charAt(1));
+
+        char[] ch = new char[str1.length()];
+        str1.getChars(0, str1.length(), ch, 0);
+        System.out.println("getChars: " + new String(ch));
+
+        System.out.println("compareTo: " + str1.compareTo(str2));
+        System.out.println("equals: " + str1.equals(str2));
+        System.out.println("equalsIgnoreCase: " + str1.equalsIgnoreCase(str2));
+        System.out.println("startsWith(A): " + str1.startsWith("A"));
+        System.out.println("endsWith(a): " + str1.endsWith("a"));
+        System.out.println("contains(a): " + str1.contains("a"));
+        System.out.println("indexOf(a): " + str1.indexOf('a'));
+        System.out.println("toUpperCase: " + str1.toUpperCase());
+        System.out.println("toLowerCase: " + str1.toLowerCase());
+        System.out.println("replace(a,x): " + str1.replace('a','x'));
+
+        String intInput = sc.nextLine();
+        Integer intValue = Integer.valueOf(intInput);
+        System.out.println("Integer: " + intValue);
+
+        String doubleInput = sc.nextLine();
+        Double doubleValue = Double.valueOf(doubleInput);
+        System.out.println("Double: " + doubleValue);
+
+        sc.close();
+    }
 }
-
-echo "Factorial of $num is: $fact";
-?>
-
-</body>
-</html>
-
